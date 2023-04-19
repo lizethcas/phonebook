fdffd53367362548101316a8fe3ea11f66b6b90a
Lizeth Castillo <lizcg-8@gmail.com>
2023-04-18 22:54:09 -0500

    add homepage for github page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base:'https://lizethcas.github.io/goit-react-hw-02-phonebook/'
 })
