2333141c5fbb56d0ca563980df4e35e8a6f17f27
Lizethcas <lcastil149@gmail.com>
2025-05-16 14:08:43 -0500

    Agrega configuración de GitHub Actions para implementar la aplicación en GitHub Pages, actualiza la configuración base en vite.config.js y añade el archivo .nojekyll para evitar el procesamiento de Jekyll.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://lizethcas.github.io/phonebook/'
+  base: '/phonebook/'
 })
