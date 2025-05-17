8b0ea160ccbb1bfd6f64dfd83b60231f084899d1
Lizethcas <lcastil149@gmail.com>
2025-05-16 19:53:34 -0500

    Elimina la configuración de salida del directorio 'docs' en vite.config.js y ajusta la configuración base para el despliegue en GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/phonebook/', // cambia esto por el nombre real del repo
   plugins: [react()],
-  base: '/phonebook/',
-  build: {
-    outDir: 'docs'
-  }
 })
